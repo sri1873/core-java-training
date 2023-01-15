550de34090072e8fa4efcd3e087712d313269783
Sri Kumar <binduhasitha.vasireddy@effigo.in>
2023-01-15 20:18:41 +0530

    Update abstration

--- oops/src/abstraction/Calculator.java
+++ oops/src/abstraction/Calculator.java
@@ -1,14 +1,43 @@
 package abstraction;
 
+abstract class Calculate {
+	double num1;
+	double num2;
+	char operator;
+
+	Calculate() {
+	}
+
+
+	abstract void calc(double num1, double num2, char operator);
+}
+
 public class Calculator extends Calculate {
-	public void calc();
+	public void calc(double num1, double num2, char operator) {
+		switch (operator) {
+		case '+':
+			System.out.println(num1 + num2);
+			break;
+		case '-':
+			System.out.println(num1 - num2);
+			break;
+		case '*':
+			System.out.println(num1 * num2);
+			break;
+		case '/':
+			System.out.println(num1 / num2);
+			break;
+		default:
+			System.out.println("Not in +,-,*,/");
+		}
+	};
 
 	public static void main(String[] args) {
-		Calculate ex1 = new Calculate(5, 5, '+');
-		Calculate ex2 = new Calculate(5, 5, '-');
-		Calculate ex3 = new Calculate(5, 5, '*');
-		Calculate ex4 = new Calculate(5, 5, '/');
-		Calculate ex5 = new Calculate(5, 5, '%');
+		Calculator ex1 = new Calculator();
+		Calculator ex2 = new Calculator();
+		
+		ex1.calc(10,5,'+');
+		ex2.calc(23.3, 10, '-');
 
 	}
 
